6cc685fe50f718166794cad7d42664afe8f61811
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-18 22:47:10 -0500

    Start refactoring into separate files

--- index.ios.js
+++ index.ios.js
@@ -11,11 +11,7 @@ import React, {
   TouchableHighlight,
   View,
 } from 'react-native';
-import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
-GoogleSignin.configure({
-  iosClientId: "430891231916-hej7na4spktej6ofjofis7gphtlg5op3.apps.googleusercontent.com",
-  scopes: ["https://www.googleapis.com/auth/calendar"]
-});
+import DiaryList from './diary-list'
 
 var BackgroundGeolocation = require('react-native-background-geolocation');
 BackgroundGeolocation.configure({
@@ -58,6 +54,10 @@ BackgroundGeolocation.configure({
       console.log('- [js]motionchanged: ', JSON.stringify(location));
   });
 
+  BackgroundGeolocation.on('httpresponse', (event) => {
+    console.log('- HttpResponse: ', JSON.stringify(event));
+  });
+
   BackgroundGeolocation.start(() => {
     console.log('- [js] BackgroundGeolocation started successfully');
 
@@ -69,81 +69,5 @@ BackgroundGeolocation.configure({
     });
   });
 
-class DiaryList extends Component {
-  signIn() {
-    GoogleSignin.signIn()
-    .then((user) => {
-      console.log(user);
-      this.setState({user: user});
-    })
-    .catch((err) => {
-      console.log('WRONG SIGNIN', err);
-    })
-    .done();
-  }
-  onPressButton() {
-    console.log(this)
-    fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
-      headers: {
-        "Authorization": "Bearer " + this.state.user.accessToken
-      }
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      this.setState( {events: json } )
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Text style={styles.instructions}>
-          Click the button below to log in with your Google account. If you are already logged in you may not see the Google OAuth popup.
-          Your user details should be logged in Chrome.
-        </Text>
-        <GoogleSigninButton
-            style={{width: 48, height: 48}}
-            size={GoogleSigninButton.Size.Icon}
-            color={GoogleSigninButton.Color.Dark}
-            onPress={this.signIn.bind(this)}
-        />
-        <Text style={styles.instructions}>
-          Once logged in, retrieve your primary calendar by clicking below.
-          Watch the Chrome JS console to see the result
-        </Text>
-
-        <TouchableHighlight onPress={this.onPressButton.bind(this)}>
-            <Text style={styles.instructions}>
-              Click here to grab calendar events
-            </Text>
-          </TouchableHighlight>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    margin: 10,
-  },
-});
 
 AppRegistry.registerComponent('DiaryList', () => DiaryList);
